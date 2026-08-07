375404798877d7c8ad7e541fe29fef9cc0ab87f6
Conor <conorkenahan@gmail.com>
2026-08-07 17:44:32 -0400

    feat: added allocation math and input parsing

---
+++ src/lib/allocate.js
@@ -0,0 +1,26 @@
+import { MAX_AMOUNT } from '../config/assets.js'
+
+export function allocate(usdAmount, rate, percent) {
+  const usd = usdAmount * (percent / 100)
+  const crypto = usd * rate
+
+  return { usd, crypto }
+}
+
+export function parseAmount(input) {
+  const cleaned = String(input).replace(/[$,\s]/g, '')
+  if (cleaned === '') return null
+  return Number(cleaned)
+}
+
+export function validateAmount(input) {
+  const value = parseAmount(input)
+
+  if (value === null) return null
+  if (Number.isNaN(value)) return 'Enter a number.'
+  if (value < 0) return 'Enter an amount of 0 or more.'
+  if (value > MAX_AMOUNT) {
+    return `Enter an amount under ${MAX_AMOUNT.toLocaleString('en-US')}.`
+  }
+  return null
+}
