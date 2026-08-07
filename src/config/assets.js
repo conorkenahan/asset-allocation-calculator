eebf1c304a06b71ecfb96d728070bb9dc20504ef
Conor <conorkenahan@gmail.com>
2026-08-07 17:43:43 -0400

    feat: added asset config

---
+++ src/config/assets.js
@@ -0,0 +1,11 @@
+export const ASSETS = {
+  BTC: { symbol: 'BTC', name: 'Bitcoin', decimals: 8 },
+  ETH: { symbol: 'ETH', name: 'Ethereum', decimals: 6 },
+}
+
+export const DEFAULT_ALLOCATION = [
+  { symbol: 'BTC', percent: 70 },
+  { symbol: 'ETH', percent: 30 },
+]
+
+export const MAX_AMOUNT = 1_000_000_000
